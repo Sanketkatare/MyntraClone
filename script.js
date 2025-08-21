d7427f0227b45edec044812abb909987cc07cbd6
Nagaraj Katare <sanketkatare063@gmail.com>
2025-08-21 23:46:13 +0530

    4th commit 21/8/25

--- script.js
+++ script.js
@@ -1,49 +1,29 @@
-// // let currentSlide = 0;
-// // const slides = document.querySelectorAll('.slide'); 
+// let currentSlide = 0;
+// const slides = document.querySelectorAll('.slide'); 
 
-// // function showSlide(index) {
-// //     slides.forEach((slide, i) => {
-// //         slide.classList.toggle('active', i === index);
-// //     });
-// // }
-// // showSlide(currentSlide);
-
-// // setInterval(() => {
-// //     currentSlide = (currentSlide + 1) % slides.length;
-// //     showSlide(currentSlide);
-// // }, 2000); // Change slide every 3 seconds
-
-// function slideshow() {
-//   // clone
-//   $('.slider-1').clone().removeClass('slider-1').addClass('slider-2').insertAfter($('.slider'));
-
-//   // set first
-//   $('.slider-1').slick({
-//     draggable: false,
-//     dots: false,
-//     infinite: true,
-//     responsive: true,
-//     asNavFor: '.slider-2',
-//     touchThreshold: 20,
-//     speed: 1000,
-//     fade: true
-//   });
-
-//   // set second
-//   $('.slider-2').slick({
-//     dots: true,
-//     infinite: true,
-//     responsive: true,
-//     asNavFor: '.slider-1',
-//     arrows: false,
-//     speed: 1000,
-//     easing: 'easeInOutQuart'
-//   });
+// function showSlide(index) {
+//     slides.forEach((slide, i) => {
+//         slide.classList.toggle('active', i === index);
+//     });
 // }
+// showSlide(currentSlide);
 
-// $(function() {
-//   slideshow();
-//   setTimeout(function() {
-//     $('.slider-1 .slick-next').click();
-//   }, 1000);
-// })
+// setInterval(() => {
+//     currentSlide = (currentSlide + 1) % slides.length;
+//     showSlide(currentSlide);
+// }, 2000); // Change slide every 3 seconds
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide'); 
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+showSlide(currentSlide);
+
+setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}, 3000); // Change slide every 3 seconds
